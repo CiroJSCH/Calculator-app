a469d60548a3ff5d6cc80cbe469cf6afb205f59c
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2023-02-07 18:53:02 -0300

    feat: Theme values for all the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-type Props = {}
+// Libraries
+import { useState } from 'react';
+
+// Components
+import ThemeSwitcher from './components/ThemeSwitcher';
+
+type Props = {};
 
 const App = (props: Props) => {
-  return (
-    <div className="text-red-500">App</div>
-  )
-}
+  const [theme, setTheme] = useState<number>(3);
 
-export default App
+  const themes: {[value: number] : string} = {
+    1: 'theme-first',
+    2: 'theme-second',
+    3: 'theme-third',
+  };
+
+  return (
+    <main className={`${themes[theme]} h-screen bg-main px-8 py-9`}>
+      <ThemeSwitcher theme={theme} setTheme={setTheme}/>
+    </main>
+  );
+};
+
+export default App;
