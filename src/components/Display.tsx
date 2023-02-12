aa1b36ba779c65f306448a43f0163dabd754d8d4
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2023-02-12 09:22:17 -0300

    responsive display

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -2,23 +2,30 @@
 import { useContext } from 'react';
 
 // Context
-import { ThemeContext, ThemeContextType, ThemeType } from '../context/ThemeContext';
-import { CalculatorContext, CalculatorContextType } from '../context/CalculatorContext';
+import {
+  ThemeContext,
+  ThemeContextType,
+  ThemeType,
+} from '../context/ThemeContext';
+import {
+  CalculatorContext,
+  CalculatorContextType,
+} from '../context/CalculatorContext';
 
 const Display = () => {
   const { theme } = useContext(ThemeContext) as ThemeContextType;
-  const { result, operation } = useContext(CalculatorContext) as CalculatorContextType;
+  const { result, operation } = useContext(
+    CalculatorContext
+  ) as CalculatorContextType;
 
   return (
-    <div className='w-full h-auto bg-screen rounded-lg mt-8 px-6 py-5 flex items-center justify-end'>
+    <div className='w-full h-auto bg-screen rounded-lg mt-8 px-6 xl:py-5 py-4 flex items-center justify-end'>
       <span
-        className={`text-[45px] text-txtColor font-bold overflow-x-scroll scrollbar-thin scrollbar-thumb-keySecSh ${
-          theme === 1 as ThemeType  ? 'text-txtWhite' : 'text-txtColor'
+        className={`xl:text-[45px] text-[40px] text-txtColor font-bold overflow-x-scroll scrollbar-thin scrollbar-thumb-keySecSh ${
+          theme === (1 as ThemeType) ? 'text-txtWhite' : 'text-txtColor'
         }`}
       >
-        {
-          operation !== "" ? operation : result
-        }
+        {operation !== '' ? operation : result}
       </span>
     </div>
   );
